84687cff29ffb9193bd171d29ab548680b1f0302
d.sabanin <d.sabanin@s7.ru>
2026-05-18 00:27:53 +0400

    step_1

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter } from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+        <h1 className="text-4xl font-bold text-gray-800">Quiz Game</h1>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
